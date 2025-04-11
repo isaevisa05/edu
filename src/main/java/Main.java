ac1a0a5cd943f4c660d26ba374595c15dc465e96
isaevisa05 <isaevisao55@gmail.com>
2025-04-11 13:49:13 +0300

    u

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,7 @@
-import java.util.Objects;
-
 public class Main {
 
     public static void main(String[] args) {
-        Thread serverThread = new Thread(new Server());
-        serverThread.start();
+        
     }
 
 }
